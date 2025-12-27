e9332d9c01522edc623c2629a740fd27ceaeda3d
Gregory Raevski <gregory.raevski@gmail.com>
2025-12-27 15:27:59 +1100

    Add markers to the graph

--- www/main.js
+++ www/main.js
@@ -77,60 +77,6 @@ function initSettingsView() {
 var offset = 0.0;
 var scale = 1.0;
 
-function initDataView() {
-    /** @type {SVGSVGElement} */ const dataGraph = document.getElementById("data-graph");
-    /** @type {SVGPolylineElement} */ const dataGraphLine = document.getElementById("data-graph-trace");
-    /** @type {HTMLElement} */ const dataCurrentText = document.getElementById("data-current");
-    /** @type {HTMLElement} */ const dataMaxText = document.getElementById("data-max");
-
-    var x = 0;
-    function updateLiveGraph(chunk) {
-        const viewBox = dataGraph.viewBox.baseVal;
-        for (const v of chunk) {
-            const point = dataGraph.createSVGPoint();
-            point.x = ++x;
-            point.y = v;
-            dataGraphLine.points.appendItem(point);
-            if (dataGraphLine.points.length > viewBox.width) {
-                dataGraphLine.points.removeItem(0);
-            }
-        }
-        const graphLineMatrix = dataGraph.createSVGMatrix();
-        graphLineMatrix.d = -1;
-        graphLineMatrix.e = viewBox.x+viewBox.width-x;
-        graphLineMatrix.f = viewBox.y+viewBox.height;
-        dataGraphLine.transform.baseVal.replaceItem(dataGraphLine.transform.baseVal.createSVGTransformFromMatrix(graphLineMatrix), 0);
-    }
-
-    function updateLiveView(chunk) {
-        updateLiveGraph(chunk);
-        for (let value of chunk) {
-            if (value > maxValue) {
-                maxValue = value;
-            }
-        }
-        dataCurrentText.textContent = ((chunk[chunk.length-1]+offset)/scale).toFixed(2);
-        dataMaxText.textContent = ((maxValue+offset)/scale).toFixed(2);
-    }
-
-    offset = parseFloat(localStorage.getItem("calibration-offset")) || 0.0;
-    scale = parseFloat(localStorage.getItem("calibration-scale")) || 1.0;
-
-    let maxValue = offset;
-    document.getElementById("data-max-reset").onclick = () => {
-        maxValue = offset; 
-        dataMaxText.textContent = "0.00";
-    };
-
-    dataGraphLine.transform.baseVal.appendItem(dataGraph.createSVGTransformFromMatrix(dataGraph.createSVGMatrix()));
-    const calibrationMatrix = dataGraph.createSVGMatrix();
-    calibrationMatrix.d = 1.0/scale;
-    calibrationMatrix.f = -offset*calibrationMatrix.d;
-    dataGraphLine.transform.baseVal.appendItem(dataGraph.createSVGTransformFromMatrix(calibrationMatrix));
-
-    fetchData("/load.bin", updateLiveView);
-}
-
 function initCalibrationView() {
     /** @type {HTMLInputElement} */ const calibrationOffsetInput = document.getElementById("calibration-offset");
     /** @type {HTMLInputElement} */ const calibrationScaleInput = document.getElementById("calibration-scale");
@@ -149,8 +95,70 @@ function initCalibrationView() {
 
     document.getElementById("calibration-apply").onclick = applyCalibration;
     document.getElementById("calibration-reset").onclick = resetCalibration;
+
+    offset = parseFloat(localStorage.getItem("calibration-offset")) || 0.0;
+    scale = parseFloat(localStorage.getItem("calibration-scale")) || 1.0;
     
     resetCalibration();
+}
+
+function initDataView() {
+    /** @type {SVGSVGElement} */ const dataGraph = document.getElementById("data-graph");
+    /** @type {SVGPolylineElement} */ const dataGraphLine = document.getElementById("data-graph-trace");
+    /** @type {SVGLineElement} */ const dataGraphMaxLine = document.getElementById("data-graph-max-line");
+    /** @type {SVGTextElement} */ const dataGraphMaxText = document.getElementById("data-graph-max");
+    /** @type {SVGLineElement} */ const dataGraphCurrentLine = document.getElementById("data-graph-current-line");
+    /** @type {SVGTextElement} */ const dataGraphCurrentText = document.getElementById("data-graph-current");
+    /** @type {HTMLElement} */ const dataCurrentText = document.getElementById("data-current");
+    /** @type {HTMLElement} */ const dataMaxText = document.getElementById("data-max");
+
+    var x = 0;
+    function updateLiveGraph(chunk) {
+        const viewBox = dataGraph.viewBox.baseVal;
+        for (const v of chunk) {
+            const point = dataGraph.createSVGPoint();
+            point.x = ++x;
+            point.y = v;
+            dataGraphLine.points.appendItem(point);
+            if (dataGraphLine.points.length > viewBox.width) {
+                dataGraphLine.points.removeItem(0);
+            }
+        }
+        const graphLineMatrix = dataGraph.createSVGMatrix();
+        graphLineMatrix.d = 1.0/scale;
+        graphLineMatrix.f = -offset*graphLineMatrix.d;
+        graphLineMatrix.e = viewBox.x+viewBox.width-x;
+        dataGraphLine.transform.baseVal.initialize(dataGraph.createSVGTransformFromMatrix(graphLineMatrix));
+    }
+
+    function updateLiveView(chunk) {
+        updateLiveGraph(chunk);
+        for (let value of chunk) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+        const calibratedCurrent = ((chunk[chunk.length-1]-offset)/scale);
+        dataCurrentText.textContent = calibratedCurrent.toFixed(2).padStart(6);
+        dataGraphCurrentLine.y1.baseVal.value = dataGraphCurrentLine.y2.baseVal.value = calibratedCurrent;
+        dataGraphCurrentText.y.baseVal.getItem(0).value = -calibratedCurrent-1;
+        dataGraphCurrentText.textContent = calibratedCurrent.toFixed(2);
+
+        const calibratedMax = ((maxValue-offset)/scale);
+        dataMaxText.textContent = calibratedMax.toFixed(2).padStart(6, ' ');
+        dataGraphMaxLine.y1.baseVal.value = dataGraphMaxLine.y2.baseVal.value = calibratedMax;
+        dataGraphMaxText.y.baseVal.getItem(0).value = -calibratedMax-1;
+        dataGraphMaxText.textContent = calibratedMax.toFixed(2);
+    }
+
+    let maxValue = offset;
+    document.getElementById("data-max-reset").onclick = () => {
+        maxValue = offset; 
+        dataMaxText.textContent = "0.00";
+    };
+
+    fetchData("/load.bin", updateLiveView);
+    //window.setInterval(() => updateLiveView([Math.random()*5000-50]), 100);
 }
 
 initNavigation();
